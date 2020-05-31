1f6ecd9560a20092b814945581ea4bd0e804a020
AlexKatros <alexkatros@gmail.com>
2020-05-31 16:58:48 -0400

    data table added

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,45 +1,41 @@
+angular
+	.module('Challenge', [])
+	.controller('ChallengeController', ChallengeCtrl);
 
+ChallengeCtrl.$inject = ['$http', '$scope'];
 
-angular.module("Challenge", [])
-	.controller("ChallengeController", ChallengeCtrl);
-
-ChallengeCtrl.$inject = ["$http"];
-
-function ChallengeCtrl($http) {
+function ChallengeCtrl($http, $scope) {
 	var cCtrl = this;
+	console.log('Controller loaded!');
+	cCtrl.welcomeMessage = 'Welcome to the challenge!';
+	getShipmentData();
 
-	console.log("Controller loaded!");
-	cCtrl.welcomeMessage = "Welcome to the challenge!";
-
-	//load JSON from url
-	$.getJSON('https://automation.bigdaddyunlimited.com/tracking_data.json', function (data) {
-
-		//Loop through json file and make a POST request for each index
-		for (var i = 0; i < data.length; i++) {
-			var obj = data[i];
-
-
-			$.ajax({
-				type: 'POST',
-				url: 'http://localhost:3000',
-				data: JSON.stringify(obj),
-				contentType: 'application/json',
-				dataType: 'json',
-				success: console.log(obj)
-			});
-
-		}
-		var date = new Date();
-		var deletionDate = new Date(date.setDate(date.getDate() - 3));
-		db.shipments.remove({ insertDate: { $lt: deletionDate } });
-
-
-
+	$http.get('/shipments').then(function (res) {
+		$scope.shipments = res.data;
 	});
-
-
-
-
 }
 
+//Add JSON data to MongoDB with POST requests
+function getShipmentData() {
+	$.getJSON(
+		'https://automation.bigdaddyunlimited.com/tracking_data.json',
+		function (data) {
+			//Loop through json file and make a POST request for each index
+			for (var i = 0; i < data.length; i++) {
+				var obj = data[i];
 
+				$.ajax({
+					type: 'POST',
+					url: 'http://localhost:3000',
+					data: JSON.stringify(obj),
+					contentType: 'application/json',
+					dataType: 'json',
+					success: console.log(obj),
+				});
+			}
+			// var date = new Date();
+			// var deletionDate = new Date(date.setDate(date.getDate() - 3));
+			// db.shipments.remove({ insertDate: { $lt: deletionDate } });
+		}
+	);
+}
--- routes.js
+++ routes.js
@@ -2,20 +2,29 @@ let request = require('request');
 const express = require('express'),
 	Shipment = require('./models/ShipmentSchema');
 
-
 module.exports = (app) => {
 	// home route - serves angular application
-	app.get("/", (req, res) => {
-		res.sendFile("app.html", { root: __dirname + "/public/html" });
+	app.get('/', (req, res) => {
+		res.sendFile('app.html', { root: __dirname + '/public/html' });
 	});
 
-	app.post("/", async (req, res) => {
+	app.get('/shipments', async (req, res) => {
+		try {
+			const shipments = await Shipment.find();
+			res.json(shipments);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	});
+
+	app.post('/', async (req, res) => {
 		const {
 			ship_date,
 			tracking_number,
 			shipping_carrier,
 			shipping_method,
-			tracking_url
+			tracking_url,
 		} = req.body;
 
 		const shipmentFields = {};
@@ -26,22 +35,16 @@ module.exports = (app) => {
 		if (tracking_url) shipmentFields.tracking_url = tracking_url;
 
 		try {
-
 			let shipment = await Shipment.findOneAndUpdate(
 				{ tracking_number: req.body.tracking_number },
 				{ $set: shipmentFields },
 				{ new: true, upsert: true }
-
 			);
 			await shipment.save();
 			res.json(shipment);
-
-
-
 		} catch (err) {
 			console.error(err.message);
 			res.status(500).send('Server Error');
 		}
-
-	})
+	});
 };
